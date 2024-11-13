df486c5892e20fa3268e00d2578d986f6803546b
Wagner Cardoso <wagnerdev01@gmail.com>
2024-11-12 22:29:38 -0300

    feat(infra): add cors config

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,10 +1,16 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 import { welcomeRoutes } from './routes/welcome';
 import { budgetsRoutes } from './routes/budgets';
 import { ResourceNotFoundError } from '@/app/errors/resource-not-found';
 
 const app = Fastify({
   logger: true,
+});
+
+app.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 });
 
 app.setErrorHandler(async (error, request, reply) => {
