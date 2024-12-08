043bc23b01684462371a39d8f0df7d57838b7bf9
Wagner Cardoso <wagnerdev01@gmail.com>
2024-12-08 14:04:48 -0300

    chore: build config

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,4 +1,8 @@
 import { app } from './app';
 import { env } from './config/env';
 
-app.listen({ port: env.PORT }).then(() => console.log('Server is running'));
+app
+  .listen({ port: env.PORT, host: env.HOST })
+  .then(() =>
+    console.log(`Server is running on host ${env.HOST} and port ${env.PORT}`),
+  );
